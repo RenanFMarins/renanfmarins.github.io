68e2de576cb7e6b773d6e949084bd03b883bb7de
RenanFMarins <105497014+RenanFMarins@users.noreply.github.com>
2022-11-18 16:45:42 -0300

    Add files via upload

---
+++ java/mudar.js
@@ -0,0 +1,13 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.meu_banner', {
+    delay:300
+});
+
+ScrollReveal().reveal('.sobre_eu', {
+    delay:500
+})
+
+ScrollReveal().reveal('.contatos', {
+    delay:700
+});
